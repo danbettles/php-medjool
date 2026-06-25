69551d049d4359ba6142d1cf438302a841e266b9
Daniel Bettles <danbettles@yahoo.co.uk>
2026-06-25 18:21:15 +0100

    Add conspicuous notice about time-zone used in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
 
-date_default_timezone_set('UTC');
+$timeZoneForTests = 'UTC';
+
+date_default_timezone_set($timeZoneForTests);
+
+// phpcs:ignore DanBettles.Debug.PHPOutputFunctions.Found
+echo "\e[97;41m ㊟ Time-zone set to `{$timeZoneForTests}` for tests \e[0m\n\n";
